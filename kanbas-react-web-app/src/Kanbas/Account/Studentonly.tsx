92f3c0e4f969893d3a8cb559d377902ff52a0ab6
Yanzhengnb <91861377+Yanzhengnb@users.noreply.github.com>
2024-11-05 00:14:20 -0500

    Delete kanbas-react-web-app directory

--- kanbas-react-web-app/src/Kanbas/Account/Studentonly.tsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-export default function Studentonly({ children }: { children: any }) {
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-  if (currentUser?.role==="STUDENT") {
-    return children;
-  }
-  else {
-    return null;
-}}
